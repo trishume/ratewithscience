1cdb44eb8636db5597618f8c68d5bbff54d88635
Tristan Hume <tris.hume@gmail.com>
2015-03-27 19:42:00 -0400

    Add api json

--- server.nim
+++ server.nim
@@ -1,8 +1,13 @@
 # example.nim
-import jester, asyncdispatch
+import jester, asyncdispatch, json, sequtils
 
 routes:
-  get "/test":
-    resp "Hello world"
+  get "/api/findscale":
+    let start = request.params["start"]
+    let stop = request.params["stop"]
+    var path = @["lol","wut","hi",start, stop]
+    let pathj = %mapIt(path,JsonNode, %it)
+    let q = 2
+    resp "{\"status\":\"ok\", \"scale\": "& $pathj &", \"quality\": "& $q &"}"
 
 runForever()
