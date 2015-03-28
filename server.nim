e7cd9b18de16b458cf5592e3ac600741e339df13
Tristan Hume <tris.hume@gmail.com>
2015-03-27 23:43:14 -0400

    More Nim stuff

--- server.nim
+++ server.nim
@@ -1,13 +1,31 @@
 # example.nim
-import jester, asyncdispatch, json, sequtils
+# import jester, asyncdispatch, json, sequtils
+import jester, asyncdispatch
+# import lib/graph
+
+# var
+#   g : Graph
+#   mapper : Mapper
 
 routes:
   get "/api/findscale":
-    let start = request.params["start"]
-    let stop = request.params["stop"]
-    var path = @["lol","wut","hi",start, stop]
-    let pathj = %mapIt(path,JsonNode, %it)
-    let q = 2
-    resp "{\"status\":\"ok\", \"scale\": "& $pathj &", \"quality\": "& $q &"}"
+    # var g = load_bin_graph()
+    # var mapper = init_mapper()
+    # let start = request.params["start"]
+    # let stop = request.params["stop"]
+    # echo "Pathing..."
+    # var res = find_path_s(g,mapper,start,stop)
+    # echo "Done Pathing..."
+    # if not res.worked: resp "{\"scale\":\"bad\"}"
+    # let path = @["hi"]
+    # let pathj = %mapIt(path,JsonNode, %it)
+    # let q = 1
+    # let q = if res.bid: 2 else: 1
+    # resp "{\"status\":\"ok\", \"scale\": "& $pathj &", \"quality\": "& $q &"}"
+    resp "Hello World"
 
+# echo "Loading..."
+# g = load_bin_graph()
+# mapper = init_mapper()
+# echo "Serving..."
 runForever()
