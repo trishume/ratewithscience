124c64486abd3261389bba71dc393bf87f2059da
Tristan Hume <tris.hume@gmail.com>
2015-03-28 20:49:17 -0400

    Add locking in case of concurrency

--- server.nim
+++ server.nim
@@ -1,10 +1,11 @@
 # example.nim
-import jester/jester, asyncdispatch, json, sequtils
+import jester/jester, asyncdispatch, json, sequtils, locks
 import lib/graph
 
 var
   g : Graph
   mapper : Mapper
+  mutex : TLock
 
 routes:
   get "/api/findscale":
@@ -13,7 +14,9 @@ routes:
     let start = request.params["start"]
     let stop = request.params["stop"]
     echo "Pathing..."
+    acquire mutex
     var res = find_path_s(g,mapper,start,stop)
+    release mutex
     echo "Done Pathing..."
     if not res.worked: resp "{\"scale\":\"bad\"}"
     # let path = @["hi"]
@@ -24,8 +27,11 @@ routes:
     resp "{\"status\":\"ok\", \"scale\": "& $pathj &", \"quality\": "& $q &"}"
     # resp "Hello World"
 
+initLock mutex
 echo "Loading..."
+acquire mutex
 g = load_bin_graph()
 mapper = init_mapper()
+release mutex
 echo "Serving..."
 runForever()
